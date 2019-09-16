e205006bc4ee1ca5e7fe74be85742fb49a72be05
yazan <yazan.mehrez@gmail.com>
2019-09-17 02:32:36 +0400

    responsive navbar

--- src/app/views/home/partners/partners.component.ts
+++ src/app/views/home/partners/partners.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
+import {tns} from 'tiny-slider/src/tiny-slider';
 
 @Component({
-  selector: 'dcaa-partners',
-  templateUrl: './partners.component.html',
-  styleUrls: ['./partners.component.scss']
+    selector: 'dcaa-partners',
+    templateUrl: './partners.component.html',
+    styleUrls: ['./partners.component.scss']
 })
-export class PartnersComponent implements OnInit {
+export class PartnersComponent implements AfterViewInit {
 
-  constructor() { }
+    partners = [
+        'assets/images/partners/al ameen.png',
+        'assets/images/partners/ask.png',
+        'assets/images/partners/complain.png',
+        'assets/images/partners/dubai career.png',
+        'assets/images/partners/e-suggest-logo.png',
+        'assets/images/partners/expo 2020.png',
+        'assets/images/partners/gov ae.png',
+        'assets/images/partners/mmrz.png',
+        'assets/images/partners/smart dubai.png',
+    ];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
+
+    ngAfterViewInit() {
+        tns({
+            container: '.partners',
+            items: 7,
+            mode: 'carousel',
+            controls: false,
+            slideBy: 1,
+            controlsContainer: false,
+            nav: false,
+            mouseDrag: true,
+            autoplayButtonOutput: false,
+            autoplay: true,
+            loop: false,
+            rewind: true
+        });
+
+        const live_pagination = document.getElementsByClassName('tns-liveregion');
+        live_pagination[0].remove();
+    }
 
 }
